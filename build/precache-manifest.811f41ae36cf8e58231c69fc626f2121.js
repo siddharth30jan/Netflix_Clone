ce13b46ab6a3dbf89479a3ab47dedb7a14db3f05
Siddharth Singh <siddharths51700@gmail.com>
2020-09-30 18:21:34 +0530

    ready to deploy

---
+++ build/precache-manifest.811f41ae36cf8e58231c69fc626f2121.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd9abee1199327911e9a2539a9c1e1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df90309514deb4656178",
+    "url": "/static/css/main.64f5a69d.chunk.css"
+  },
+  {
+    "revision": "2421364a8c3b4a6f9cec",
+    "url": "/static/js/2.6d2c9468.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.6d2c9468.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df90309514deb4656178",
+    "url": "/static/js/main.b68c9418.chunk.js"
+  },
+  {
+    "revision": "5995777ecf96f1993615",
+    "url": "/static/js/runtime-main.d76bb10c.js"
+  }
+]);
